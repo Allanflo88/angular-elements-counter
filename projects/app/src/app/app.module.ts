657d02949526a9261e5767e0c1db6778c7a630c5
allanflo88 <allanfloresdejesus@hotmail.com>
2022-02-12 12:12:16 -0300

    try again

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LazyElementsModule } from '@angular-extensions/elements';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    LazyElementsModule 
   ],
   providers: [],
   bootstrap: [AppComponent],
