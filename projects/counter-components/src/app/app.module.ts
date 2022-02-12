343e23a0504844e922f946052b7a6c97444074f5
allanflo88 <allanfloresdejesus@hotmail.com>
2022-02-12 12:01:42 -0300

    remove dynamic imports

--- projects/counter-components/src/app/app.module.ts
+++ projects/counter-components/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ export class AppModule {
   }
 
   async createComponents(): Promise<void> {
-    const {TitleComponent} = await import('./title/title.component');
+    // const {TitleComponent} = await import('./title/title.component');
     const customElement = createCustomElement(TitleComponent, {
       injector: this._injector
     })
